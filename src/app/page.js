bf917dad1800bc96235aef0aba67dede5258d4d4
aungmyomyat <aungmyomyat874@gmail.com>
2024-02-07 06:45:56 +0630

    Initial commit

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import HomePage from '../app/component/home/page'
+
+export default function Home() {
+  return (
+    <div className={'w-full h-screen flex justify-center items-center'}>
+      <HomePage></HomePage>
+    </div>
+  );
+}
