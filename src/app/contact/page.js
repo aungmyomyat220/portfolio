fd21cfa1c55d610043fc9fedf59ebd6200c7ed2d
aungmyomyat220 <aungmyomyat874@gmail.com>
2024-02-25 09:52:10 +0630

    add loading in contact section

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -10,9 +10,11 @@ import {
   AlertDescription,
   AlertTitle,
 } from "@/components/ui/alert"
+import {log} from "next/dist/server/typescript/utils";
 
 const Page = ({ theme }) => {
   const [noti, showNoti] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [emailData, setEmailData] = useState({
     email: "",
     name: "",
@@ -28,9 +30,11 @@ const Page = ({ theme }) => {
 
   const handleClick = async() => {
     if (emailData.email || emailData.subject|| emailData.content) {
+      setLoading(true)
       const response = await sendMailHook(emailData)
       if(response.statusCode === 200){
         showNoti(true)
+        setLoading(false)
         setTimeout(() => {
           showNoti(false);
         }, 4000);
@@ -131,25 +135,44 @@ const Page = ({ theme }) => {
                   placeholder={"Tell us more about your project"}
                 ></textarea>
 
-                <button id="send-btn" className={"mt-5"} onClick={handleClick}>
-                  <div className="svg-wrapper-1">
-                    <div className="svg-wrapper">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        width="20"
-                        height="20"
-                      >
-                        <path fill="none" d="M0 0h24v24H0z"></path>
-                        <path
-                          fill="currentColor"
-                          d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"
-                        ></path>
-                      </svg>
-                    </div>
-                  </div>
-                  <span>Send</span>
-                </button>
+                {
+                  loading ?
+                      <button className={"mt-5 bg-blue-600 rounded-lg px-3 py-1"}>
+                          <div>
+                            <div role="status" className={'px-6 py-1 rounded-lg'}>
+                              <svg aria-hidden="true"
+                                   className="w-5 h-5 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                                   viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                    fill="currentColor"/>
+                                <path
+                                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                    fill="currentFill"/>
+                              </svg>
+                          </div>
+                        </div>
+                      </button> :
+                      <button id="send-btn" className={"mt-5"} onClick={handleClick}>
+                        <div className="svg-wrapper-1">
+                          <div className="svg-wrapper">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                width="20"
+                                height="20"
+                            >
+                              <path fill="none" d="M0 0h24v24H0z"></path>
+                              <path
+                                  fill="currentColor"
+                                  d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"
+                              ></path>
+                            </svg>
+                          </div>
+                        </div>
+                        <span>Send</span>
+                      </button>
+                }
               </div>
             </div>
           </div>
